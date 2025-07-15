9f5403838fa6d89d1f7e5092f9fd5ee879a59fe7
Steve Zelaznik <steve.zelaznik@gnar.dog>
2025-07-15 16:19:00 -0500

    only debounce input text fields

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,9 +1,40 @@
 import { Controller } from "@hotwired/stimulus"
 import debounce from "debounce";
 
+DEBOUNCABLE_TYPES = [
+  'color',
+  'date',
+  'datetime-local',
+  'email',
+  'month',
+  'number',
+  'password',
+  'range',
+  'search',
+  'tel',
+  'text',
+  'time',
+  'url',
+  'week'
+]
+
 // Connects to data-controller="form"
 export default class extends Controller {
   initialize() {
-    this.submit = debounce((e) => this.element.requestSubmit(), 300);
+    this._submitText = debounce((e) => this._submit(), 350);
+  }
+
+  submit(e) {
+    var target = (e && e.target) || {};
+    var tagName = target.tagName || '';
+    if (tagName.match(/input/i) && DEBOUNCABLE_TYPES.indexOf(target.type) > -1) {
+      this._submitText();
+    } else {
+      this._submit();
+    }
+  }
+
+  _submit() {
+    this.element.requestSubmit();
   }
 }
