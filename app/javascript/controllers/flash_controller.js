0f4b1a6afb45904b151195d1ba786b8ad4547ee3
Steve Zelaznik <steve.zelaznik@gnar.dog>
2025-07-15 16:18:35 -0500

    play with the flash css animations

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="flash"
 export default class extends Controller {
   connect() {
-    this.element.style.animation = "fade-in-and-out 4s";
+    this.element.style.animation = "fade-in-quickly-and-out-slowly 4s";
     setTimeout(() => {
       this.element.remove();
     }, 4_000);
